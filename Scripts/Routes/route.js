12cbf68dc8ff6f5e5ce3ca2ae54ea3f08b39b599
Thineskumar Thilakanathan <skwebbies@hotmail.co.uk>
2016-09-26 10:50:46 +0200

    routing added

---
+++ Scripts/Routes/route.js
@@ -0,0 +1,40 @@
+﻿angular.module("productListing")
+    .config([
+        "$routeProvider",
+        "$locationProvider",
+        function($routeProvider, $locationProvider) {
+            $locationProvider.html5Mode(true);
+
+            $routeProvider
+                .when("/",
+                {
+                    templateUrl: "Views/Home.html",
+                    controller: "HomeController",
+                    caseInsensitiveMatch: true,
+                    activeTab: "Home"
+
+                })
+                .when("/Products",
+                {
+                    templateUrl: "Views/Products.html",
+                    controller: "ProductsController",
+                    caseInsensitiveMatch: true,
+                    activeTab: "Products"
+                })
+                .when("/Products/:id",
+                {
+                    templateUrl: "Views/Product.html",
+                    controller: "ProductController",
+                    caseInsensitiveMatch: true,
+                    activeTab: "Products"
+
+                })
+                .when("/About",
+                {
+                    templateUrl: "Views/About.html",
+                    controller: "AboutController",
+                    caseInsensitiveMatch: true,
+                    activeTab: "About"
+                });
+        }
+    ]);
